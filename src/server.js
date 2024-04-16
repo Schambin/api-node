5a05d398c4ec7f7d2188bf829af52a6b3079f5e7
Kauan <raidoka546@gmail.com>
2024-04-16 14:41:58 -0400

    learned differences between query and route params

--- src/server.js
+++ src/server.js
@@ -7,9 +7,16 @@ app.listen(PORT, () => {
     console.log(`Servidor rodando na porta ${PORT}`);
 })
 
-
+//Route Params
 app.get('/message/:user/:id', (request, response) => {
     const { user, id } = request.params;
 
     response.send(`Na url está ${user} e ${id}`);
+})
+
+//Query Params
+app.get('/products', (request, response) => {
+    const { page, limit } = request.query;
+
+    response.send(`Página ${page}. Mensagens: ${limit}`)
 })
