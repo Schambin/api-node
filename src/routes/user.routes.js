3b60363af9d28903d204490d12929b9f501cb77d
Kauan <raidoka546@gmail.com>
2024-04-29 14:19:17 -0400

    controller n routes to add, see, list and delete notes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -4,11 +4,6 @@ const UsersController = require('../controllers/users-controller');
 
 const usersRoutes = Router();
 
-function myMiddleware(req, res, next) {
-
-    next();
-}
-
 const usersController = new UsersController();
 
 usersRoutes.post('/', usersController.create);
